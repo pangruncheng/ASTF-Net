01d28138bc02717955d38c3022fcd51df5cef074
Ge Li <ge.li2@mail.mcgill.ca>
2026-04-14 10:09:26 -0400

    Simplified imports

--- astfnet/models/__init__.py
+++ astfnet/models/__init__.py
@@ -1,16 +1,5 @@
 """Neural network models for ASTF-net."""
 
-from .backbone import build_backbone, list_backbones
 from .base import ASTFModule
-from .optimizer import OPTIMIZER_REGISTRY, OptimizerFactory
-from .scheduler import SCHEDULER_REGISTRY, SchedulerFactory
 
-__all__ = [
-    "ASTFModule",
-    "build_backbone",
-    "list_backbones",
-    "OptimizerFactory",
-    "OPTIMIZER_REGISTRY",
-    "SchedulerFactory",
-    "SCHEDULER_REGISTRY",
-]
+__all__ = ["ASTFModule"]
